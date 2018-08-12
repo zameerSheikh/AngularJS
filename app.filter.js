38550e611267fe8e291b8b046a016811a47781d4
zameer <zameer@gmail.com>
2018-08-12 13:08:41 +0530

    custom filters

---
+++ app.filter.js
@@ -0,0 +1,23 @@
+"use strict"
+
+testApp.filter('ordinal', ordinal);
+
+function ordinal(){
+    return function(inputNumber){
+        if(isNaN(inputNumber)){
+            return inputNumber + 'input is not a number..'
+        }else{
+            var lastDigit = inputNumber%10;
+
+            if(lastDigit == 1){
+                return inputNumber + 'st';
+            }else if(lastDigit == 2){
+                return inputNumber + 'nd';
+            }else if(lastDigit == 3){
+                return inputNumber + 'rd';
+            }else if(lastDigit > 3){
+                return inputNumber + 'th';
+            }
+        }
+    };
+};
