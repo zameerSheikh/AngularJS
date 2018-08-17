e0fde37e5de1c3a5e6eda3e738f81b9dc972e0ef
zameer <zameer@ahex.co.in>
2018-08-17 16:47:17 +0530

    added custom directive

---
+++ directives/myDirective.js
@@ -0,0 +1,27 @@
+"use strict";
+
+(function(){
+
+    var firstDirective = function (){
+        return {
+            restrict: 'EA',
+            scope: {
+                data: '=',
+                func: '&'
+            },
+            template: `<h3>From a custom directive..{{second.name}}</h3>
+                        <span>{{data}}</span>
+                        <button ng-click="addData()">Add Data</button>`,
+            controller: function($scope){
+                console.log('$scope: ', $scope);
+                $scope.addData = function(){
+                    console.log('added new data');
+                    $scope.func()('CD');
+                }
+            }
+        }
+    };
+    
+    testApp.directive('firstDirective', firstDirective);
+    
+})();
--- secondPage.controller.js
+++ secondPage.controller.js
@@ -3,11 +3,24 @@
 testApp.controller('SecondCtrl', secondCtrl);
 
 secondCtrl.$inject = [
-    '$scope'
+    '$scope',
+    'someData'
 ];
 
 function secondCtrl(
-    $scope
+    $scope,
+    someData
 ){
 
+    $scope.second = {
+        name: 'Addy Osmani',
+        data: someData,
+        pushData: pushData,
+        localData: []
+    };
+
+        function pushData(name){
+        console.log('innn');
+        $scope.second.localData.push('pushed from:' + name)
+    }
 }
