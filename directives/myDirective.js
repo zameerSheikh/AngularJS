9ed2e83a41645c9aa4c28d4d1f54fa9dde055a62
zameer <zameer@ahex.co.in>
2018-10-11 19:03:36 +0530

    small changes to directive

--- directives/myDirective.js
+++ directives/myDirective.js
@@ -9,11 +9,12 @@
                 data: '=',
                 func: '&'
             },
-            template: `<h3>From a custom directive..{{second.name}}</h3>
+            template: `<h3>From a custom directive..</h3>
                         <span>{{data}}</span>
                         <button ng-click="func({da: 'From Directive'})">Add Data</button>`,
             link: function(scope, elem, attrs){
                 console.log('$scope: ', scope, elem);
+                // elem.css("background-color", "lightyellow");
             }
         }
     };
